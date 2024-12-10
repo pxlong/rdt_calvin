6652aeb4c0d2672c475995dbb8d2e087dcf691da
pxlong <pinxinlong@gmail.com>
2024-12-10 18:47:33 +0800

    remove .sh

--- evaluate_rdt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# export MESA_GL_VERSION_OVERRIDE=3.3
-
-python3 evaluate_calvin.py \
-    --eval_dir exps/calvin/task_d_d \
-    --configs_path configs/eval_configs.json \
-    --dataset_dir /mnt/petrelfs/longpinxin/data/calvin \
-    ${@:1}
